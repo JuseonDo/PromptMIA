bc646e93940b3c6427c78f8c9b971a87e8a37e9b
JuseonDo <dojuseon@gmail.com>
2025-09-27 13:22:44 +0000

    .

--- Falcon/utils/model_utils.py
+++ Falcon/utils/model_utils.py
@@ -1,12 +1,17 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import transformers
 import torch
+import dotenv
+import os
+
+dotenv.load_dotenv("/data2/PromptMIA/.env")
+model_cache_dir = os.getenv("model_cache_dir")
 
 def load_model(model_name = "tiiuae/falcon-7b-instruct"):
-    tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir="/data2/models")
+    tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=model_cache_dir)
     model = AutoModelForCausalLM.from_pretrained(
         model_name,
-        cache_dir="/data2/models",
+        cache_dir=model_cache_dir,
         torch_dtype=torch.bfloat16,
         device_map="auto",
         trust_remote_code=True
