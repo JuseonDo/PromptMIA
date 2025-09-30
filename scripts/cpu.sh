2ba0ee3fc94b3ce1f5d6ae49255af8c5534ac718
ejkiwi <eonji772@naver.com>
2025-09-30 06:27:57 +0000

    add REVERSE_TEST

--- scripts/cpu.sh
+++ scripts/cpu.sh
@@ -1,1 +1,5 @@
-CUDA_VISIBLE_DEVICES="" python test2.py --samples 64 --batch_size 16 --temperature 0.7
+CUDA_VISIBLE_DEVICES="" python REVERSE_test.py \
+    --samples 32 \
+    --batch_size 16 \
+    --temperature 0.7 \
+    --num_repeats 10
